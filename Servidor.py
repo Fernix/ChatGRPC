b52c478259eebc0c1eaeaa59980de2e9d742e28e
Paola Marai Lopez Benitez <maraibenitez@hotmail.com>
2019-11-30 21:06:02 -0600

    Cambio en el proto

---
+++ Servidor.py
@@ -0,0 +1,32 @@
+import grpc;
+import chat_pb2 as chat
+import chat_pb2_grpc as chat_grpc
+from concurrent import futures
+
+
+class ChatService(chat_grpc.ChatAdminServicer):
+    
+    def __init__(self):
+        super().__init__()
+
+    def GetMesseges(self, request_iterator, context):
+        for request in request_iterator:
+            self.mensajes = request
+            response = chat.GetChatsResponse
+            yield response
+
+    def SendMessage(self, request, context):
+       return 
+    
+    def GetChats(self, request, context):
+        return super().GetChats(request, context)
+    
+
+
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=20))
+
+chat_grpc.add_ChatAdminServicer_to_server(ChatService(),server)
+
+server.add_insecure_port('[::]:50051')
+server.start()
+server.wait_for_termination()
