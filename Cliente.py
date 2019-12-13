31aa2e9f6ca7e044d34ffa329a5e08648f5c7f1d
Fer cabrera <soy-yonotu@hotmail.com>
2019-12-12 20:15:11 -0600

    Crear chat

---
+++ Cliente.py
@@ -0,0 +1,33 @@
+import grpc;
+import chat_pb2 as chat
+import chat_pb2_grpc as chat_grpc
+
+# open a gRPC channel
+channel = grpc.insecure_channel('localhost:50051')
+
+# create a stub (client)
+stub = chat_grpc.ChatAdminStub(channel)
+
+# create a valid request message
+#film = proto_pb2.GetFilmRequest(id="")
+
+# make the call
+#response = stub.GetFilm(film)
+
+
+
+#response = stub.ListFilms(films)
+
+# et voilà
+#print(response)
+
+usuario1 = chat.Usuario(correo = "test12111@hotmail.com", username ="fernix")
+usuario2 = chat.Usuario(correo = "test21111@hotmail.com", username ="fernix3")
+usuarioChat1 = chat.usuariosEnChat(usuario = usuario1, idChat = 0, idchatprocedencia = 1)
+usuarioChat2 = chat.usuariosEnChat(usuario = usuario2, idChat = 0, idchatprocedencia = 10)
+#users = [chat.Usuario(correo = "soy@hotmail.com", username ="fernix"),chat.Usuario(correo = "soy2@hotmail.com", username ="fernix2")]
+c = chat.Chat(idchat = 1, nombre = "test121")
+creatChat = chat.CreateChatRequest(chat = c, usuariosenchat = [usuarioChat1,usuarioChat2])
+resp = stub.CreateChat(creatChat)
+print(resp)
+
--- Servidor.py
+++ Servidor.py
@@ -2,14 +2,65 @@ import grpc;
 import chat_pb2 as chat
 import chat_pb2_grpc as chat_grpc
 from concurrent import futures
-import MiddleWare.Chat
+import peewee
+database = peewee.MySQLDatabase("instawhat", host= "localhost", port = 3306, user = "root", password = "IriNando2403.")
+
+class Chat(peewee.Model):
+    idchat = peewee.AutoField(primary_key = True)
+    nombre = peewee.CharField()
+
+    class Meta:
+        database = database
+        db_table = "chat"
+
+class Usuario(peewee.Model):
+    username = peewee.CharField()
+    correo = peewee.CharField(primary_key = True)
+
+    class Meta:
+        database = database
+        db_table = "usuario"
+
+class UsuarioEnChat(peewee.Model):
+    usuario_correo = peewee.CharField()
+    chat_idchat = peewee.ForeignKeyField(Chat)
+    idchatprocedencia = peewee.IntegerField()
+    idusuariochat = peewee.AutoField(primary_key = True)
+
+    class Meta:
+        database = database
+        db_table = "usuario_en_chat"
 
 
 class ChatService(chat_grpc.ChatAdminServicer):
     
     def __init__(self):
         super().__init__()
+        
+    def CreateChat(self, request, context):
 
+        requestChat = request.chat
+        newChat = Chat.create(nombre = requestChat.nombre)
+        newChat.save()
+        print("paso 1")
+        for usuario in request.usuariosenchat:
+            query = Usuario.select().where( Usuario.correo == usuario.usuario)
+            print(query)
+            if query.count() == 0:
+                us = Usuario.create(correo = usuario.usuario.correo, username = usuario.usuario.username)
+                us.save()
+            else:
+                print("existe")
+            print("paso 2")
+            query2 = UsuarioEnChat.select()
+            print(query2)
+            uec = UsuarioEnChat.create(usuario_correo = usuario.usuario.correo, chat_idchat = newChat.idchat, idchatprocedencia = usuario.idchatprocedencia)
+            uec.save()
+            print("paso 3")
+        response = chat.CreateChatResponse(idchatServer = newChat.idchat)
+        return response
+ 
+ 
     def GetMesseges(self, request_iterator, context):
         for request in request_iterator:
             self.mensajes = request
@@ -24,6 +75,7 @@ class ChatService(chat_grpc.ChatAdminServicer):
     
 
 
+
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=20))
 
 chat_grpc.add_ChatAdminServicer_to_server(ChatService(),server)
@@ -32,5 +84,3 @@ server.add_insecure_port('[::]:50051')
 server.start()
 server.wait_for_termination()
 
-newChat = User.create(nombre = "prueba")
-newChat.save()
